1b25226a64352e9c74317cf3065638895039aa3b
fsvhino <ferrysafrizal20@gmail.com>
2022-12-03 19:14:11 +0800

    BC-533 [Task][Malahing-CORE] Page List Blog Tags with Table Preview (Tag Name, Slug)

---
+++ app/Http/Controllers/Admin/BlogTagsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\BlogTags;
+use Illuminate\Http\Request;
+
+class BlogTagsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $filter = $request->query('filter');
+        if (!empty($filter)) {
+            $tags = BlogTags::sortable()
+                ->where('blog-tags.tag_name', 'like', '%' . $filter . '%')
+                ->orWhere('blog-tags.slug', 'like', '%' . $filter . '%')
+                ->paginate(10);
+        } else {
+            $tags = BlogTags::sortable()->paginate(10);
+        }
+        return view('admin.blog-tags.index', compact('tags', 'filter'));
+    }
+}
---
+++ app/Models/BlogTags.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class BlogTags extends Model
+{
+    use HasFactory, Sortable;
+    protected $table = 'blog-tags';
+    public $sortable = [
+        'tag_name', 'slug', 'total'
+    ];
+    protected $fillable = [
+        'tag_name', 'slug', 'total'
+    ];
+}
